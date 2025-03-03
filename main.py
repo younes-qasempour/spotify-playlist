37bf2da81af3132b166fd239d5b9af7207463c39
younes_qasempour <qasempouryounes@gmail.com>
2025-03-03 17:03:28 +0330

    Working

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from bs4 import BeautifulSoup
-from datetime import datetime
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
 import requests
+import spotipy
 
-# users_date = input("Which year do you want to travel to ? Type the date in this format YYYY-MM-DD: ")
-users_date = "2010-12-03"
-url = f"https://www.billboard.com/charts/hot-100/{users_date}/"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
+load_dotenv()
 
-response = requests.get(url, headers=headers)
-response.raise_for_status()
-soup = BeautifulSoup(response.text, "html.parser")
-song_elements = soup.select(selector="div li ul h3")
-songs_name = [song.get_text().strip() for song in song_elements]
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri=os.getenv("REDIRECT_URI"),
+        scope="playlist-modify-private",
+        show_dialog=True,
+        cache_path="token.txt",
+        username=os.getenv("USER_NAME")
+    )
+)
+
+
+current_user_id = sp.current_user()['id']
+
+
+
+
+
+# # users_date = input("Which year do you want to travel to ? Type the date in this format YYYY-MM-DD: ")
+# users_date = "2010-12-03"
+# url = f"https://www.billboard.com/charts/hot-100/{users_date}/"
+# headers = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+# }
+#
+# response = requests.get(url, headers=headers)
+# response.raise_for_status()
+# soup = BeautifulSoup(response.text, "html.parser")
+# song_elements = soup.select(selector="div li ul h3")
+# songs_name = [song.get_text().strip() for song in song_elements]
